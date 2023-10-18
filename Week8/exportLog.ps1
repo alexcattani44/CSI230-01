1ca5de4b2b3e818a19097e9eae755f010ea4aa03
Alex Cattani <a.cattani44@gmail.com>
2023-10-18 13:18:31 -0400

    Added Log Export Script

---
+++ Week8/exportLog.ps1
@@ -0,0 +1,14 @@
+﻿#Set Directory
+cd C:\Users\champuser\Desktop\
+
+#Clear screen
+clear
+
+#Get Event Log
+Get-Eventlog -list
+
+#Ask User Which Logs to Export
+$exportLog = Read-host -Prompt "Select a log to export (.csv)"
+
+#Export to a csv file
+Get-EventLog -LogName $exportLog -Newest 3 | export-csv -NoTypeInformation -Path "C:\Users\champuser\Desktop\rep.csv"
