03d32f05857bd92fd67102cb64feb62df71743d7
Alex Cattani <a.cattani44@gmail.com>
2023-11-01 14:36:04 -0400

    Completed Line/Word Counter

---
+++ Week10/Line-WordCounter.ps1
@@ -0,0 +1,6 @@
+﻿cd "C:\Users\champuser\Desktop\CSI230-01\"
+$FilesToLookAt = Get-ChildItem -Recurse -Filter "*.bash"
+
+for ($i=0; $i -lt $FilesToLookAt.Length; $i++){
+Get-Content $FilesToLookAt[$i].FullName | Measure-Object -Line -Word
+}
